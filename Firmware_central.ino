6e61007a0f4375515367d3a3534840ff5b44394c
leovalderrama <leovalderramag@gmail.com>
2023-01-20 00:55:49 -0500

    Configuraciónn pines dht11

--- Firmware_central.ino
+++ Firmware_central.ino
@@ -2,16 +2,24 @@
 #include <DHT.h>
 /*
 pines Propósito general D0 - D13
-pin D0 -----
+pin D0 ----- digital --sensor temperatura y humedad
 pin D01 -----
 pin D02 -----
 pin D03 -----
 pin D04 -----
+.
+.
+.
+pin A0 --- análogo -- control de voltaje de batería 
 */
 // ************************dht11 humedad - temperatura *******************
-#define ledpin_0 0;
+
+#define dhtPin 0
+#define dhtType DHT11
+DHT dht(dhtPin, dhtType);
 
 // *********************** Barómetro ***************************************
+
 
 
 // *********************** Gas ***************************************
@@ -31,7 +39,7 @@ pin D04 -----
  void setup()
 {
     Serial.begin(115200);
-    //dht.begin();
+    dht.begin();
 }
 void loop(){
     
